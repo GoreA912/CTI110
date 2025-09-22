24118f717c74b69bd26ce6341a5679868fcc1d8e
GoreA912 <gorea2987@student.faytechcc.edu>
2025-09-21 22:56:43 -0400

    Add files via upload

---
+++ P1LAB1_GoreAlexis.py
@@ -0,0 +1,8 @@
+#Alexis Gore
+#9/21/25
+#PLAB1
+#Greeting Input
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
